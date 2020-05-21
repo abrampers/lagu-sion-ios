fb65343602b55bc5454fbfc283074b11be2cb3de
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-05-21 17:05:09 +0700

    Add MainView preview

--- LaguSion/Views/MainView.swift
+++ LaguSion/Views/MainView.swift
@@ -9,29 +9,6 @@
 import ComposableArchitecture
 import SwiftUI
 
-struct Song: Equatable, Identifiable {
-    var id: UUID
-    var isFavorite: Bool
-    var number: Int
-    var title: String
-}
-
-enum SongAction {
-    case heartTapped
-}
-
-struct SongEnvironment {
-    
-}
-
-let songReducer = Reducer<Song, SongAction, SongEnvironment> { state, action, environment in
-    switch action {
-    case .heartTapped:
-        state.isFavorite.toggle()
-        return .none
-    }
-}
-
 struct MainState: Equatable {
     var songs: [Song] = []
 }
@@ -59,9 +36,9 @@ struct MainView: View {
                 List {
                   ForEachStore(
                     self.store.scope(state: \.songs, action: MainAction.song(index:action:))
-                    ) { viewStore in
-                        NavigationLink(destination: SongView(store: viewStore)) {
-                            SongTabView(store: viewStore)
+                    ) { songViewStore in
+                        NavigationLink(destination: SongView(store: songViewStore)) {
+                            SongTabView(store: songViewStore)
                         }
                     }
                 }
@@ -71,59 +48,24 @@ struct MainView: View {
     }
 }
 
-struct SongTabView: View {
-    let store: Store<Song, SongAction>
-    
-    var body: some View {
-        WithViewStore(self.store) { viewStore in
-            HStack {
-                Text("\(viewStore.number)")
-                Text(viewStore.title)
-            }
-        }
-    }
-}
-
-struct SongView: View {
-    let store: Store<Song, SongAction>
-    
-    var body: some View {
-        WithViewStore(self.store) { viewStore in
-            NavigationView {
-                Text("SongView")
-                .navigationBarTitle("\(viewStore.number) \(viewStore.title)")
-                .navigationBarItems(trailing: Button(action: { viewStore.send(.heartTapped) }) {
-                    Image(systemName: viewStore.isFavorite ? "heart.fill" : "heart")
-                })
-            }
-        }
-    }
-}
-
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-//        MainView(store: Store(
-//                initialState: MainState(
-//                    songs: [
-//                        Song(id: UUID(), isFavorite: false, number: 1, title: "No 1"),
-//                        Song(id: UUID(), isFavorite: false, number: 2, title: "No 2"),
-//                        Song(id: UUID(), isFavorite: false, number: 3, title: "No 3"),
-//                        Song(id: UUID(), isFavorite: false, number: 4, title: "No 4"),
-//                        Song(id: UUID(), isFavorite: false, number: 5, title: "No 5"),
-//                        Song(id: UUID(), isFavorite: false, number: 6, title: "No 6"),
-//                        Song(id: UUID(), isFavorite: false, number: 7, title: "No 7"),
-//                        Song(id: UUID(), isFavorite: false, number: 8, title: "No 8"),
-//                        Song(id: UUID(), isFavorite: false, number: 9, title: "No 9")
-//                    ]
-//                ),
-//                reducer: mainReducer,
-//                environment: MainEnvironment()
-//            )
-        //        )
-        SongView(store: Store(
-            initialState: Song(id: UUID(), isFavorite: false, number: 1, title: "No 1"),
-            reducer: songReducer,
-            environment: SongEnvironment()
+        MainView(store: Store(
+                initialState: MainState(
+                    songs: [
+                        Song(id: UUID(), isFavorite: false, number: 1, title: "No 1"),
+                        Song(id: UUID(), isFavorite: false, number: 2, title: "No 2"),
+                        Song(id: UUID(), isFavorite: false, number: 3, title: "No 3"),
+                        Song(id: UUID(), isFavorite: false, number: 4, title: "No 4"),
+                        Song(id: UUID(), isFavorite: false, number: 5, title: "No 5"),
+                        Song(id: UUID(), isFavorite: false, number: 6, title: "No 6"),
+                        Song(id: UUID(), isFavorite: false, number: 7, title: "No 7"),
+                        Song(id: UUID(), isFavorite: false, number: 8, title: "No 8"),
+                        Song(id: UUID(), isFavorite: false, number: 9, title: "No 9")
+                    ]
+                ),
+                reducer: mainReducer,
+                environment: MainEnvironment()
             )
         )
     }
