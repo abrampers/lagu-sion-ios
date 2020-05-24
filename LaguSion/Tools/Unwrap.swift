52cc3a204af77426b55ba8ee1dbefe45d1928400
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-05-24 16:17:53 +0700

    Add unwrap

--- LaguSion/Tools/Unwrap.swift
+++ LaguSion/Tools/Unwrap.swift
@@ -1,13 +1,9 @@
 //
-//  String+Extension.swift
+//  Unwrap.swift
 //  LaguSion
 //
 //  Created by Abram Situmorang on 24/05/20.
 //  Copyright © 2020 Abram Situmorang. All rights reserved.
 //
 
-extension String: Identifiable {
-    public var id: String {
-        return self
-    }
-}
+import Foundation
