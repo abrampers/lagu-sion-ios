b865cdfc12ac0531e38fedc2a96a9ebac96095e4
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-07-07 13:41:54 +0700

    Rename LaguSionUITests to SnapshotUITests

--- SnapshotUITests/SnapshotUITests.swift
+++ SnapshotUITests/SnapshotUITests.swift
@@ -1,33 +1,33 @@
 //
-//  LaguSionUITests.swift
-//  LaguSionUITests
+//  SnapshotUITests.swift
+//  SnapshotUITests
 //
-//  Created by Abram Situmorang on 03/07/20.
+//  Created by Abram Situmorang on 07/07/20.
 //  Copyright © 2020 Abram Situmorang. All rights reserved.
 //
 
 import XCTest
 
-class LaguSionUITests: XCTestCase {
+class SnapshotUITests: XCTestCase {
     override func setUp() {
         let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
     }
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         
         let app = XCUIApplication()
@@ -53,7 +53,7 @@ class LaguSionUITests: XCTestCase {
         // UI tests must launch the application that they test.
         app.launch()
     }
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
