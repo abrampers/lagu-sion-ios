c9b24bf04b42c21bffd14f806e46790ccd1245af
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-07-31 18:22:30 +0700

    Add some section on SettingsView

--- LaguSion/Settings/Views.swift
+++ LaguSion/Settings/Views.swift
@@ -18,8 +18,64 @@ public struct SettingsView: View {
     
     public var body: some View {
         NavigationView {
-            Text("HAHA")
+            List {
+                Section {
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "airplane").font(.callout).foregroundColor(.white)
+                            }.frame(width: 28, height: 28).background(Color.orange).cornerRadius(6)
+                            Text("Airplane Mode")
+                        }
+                    }
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "wifi").font(.callout).foregroundColor(.white)
+                            }.frame(width: 28, height: 28).background(Color.blue).cornerRadius(6)
+                            Text("Wi-Fi")
+                        }
+                    }
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "wifi").font(.callout).foregroundColor(.white)
+                            }.frame(width: 28, height: 28).background(Color.blue).cornerRadius(6)
+                            Text("Bluetooth")
+                        }
+                    }
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "phone.fill").font(.callout).foregroundColor(.white)
+                            }.frame(width: 28, height: 28).background(Color.green).cornerRadius(6)
+                            Text("Cellular")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "app.badge").font(.callout).foregroundColor(.white)
+                            }.frame(width: 28, height: 28).background(Color.red).cornerRadius(6)
+                            Text("Notifications")
+                        }
+                    }
+                    NavigationLink(destination: Text("Detail View")) {
+                        HStack {
+                            ZStack {
+                                Image(systemName: "gear").font(.callout).foregroundColor(.white)
+                            }.frame(width: 28, height: 28).background(Color.gray).cornerRadius(6)
+                            Text("General")
+                        }
+                    }
+                }
+            }
+            .accentColor(.white)
             .navigationBarTitle(Text("Settings"))
+            .listStyle(GroupedListStyle())
         }
     }
 }
